12de91554eef0fa6602a45b027384d1633247935
piercingecho <107562412+piercingecho@users.noreply.github.com>
2022-12-29 17:54:31 -0500

    Added tourney parameter functionality to schedule
    SoS and PG can be used as arguments for schedule in order to create reactable messages specifically for Swim or Sink and Proving Grounds, respectively.

--- schedule.py
+++ schedule.py
@@ -11,11 +11,35 @@ class Schedule(commands.Cog):
     @discord.slash_command(
         name = "schedule",
         guild_ids=[os.getenv('TEAM_GUILD')],
-        description = "Generates one week of a team schedule.")
-        
-    async def schedule(self, ctx):
+        description = "Generates one week of a team schedule."
+        )
+    @discord.option(
+        "tourney",
+        str,
+        description = "Include Swim or Sink/Proving Grounds?",
+        required = False,
+        default = ''
+    )
+    async def schedule(self, ctx, tourney):
         d = datetime.now()
         await ctx.response.send_message("Schedule for next seven days:")
+        
+        #check for specific parameters
+        if tourney is not None:
+            if "sos" in tourney.lower():
+                sosday = d 
+                #add days so until wednesday (the date of SoS)
+                sosday += timedelta(days = ((7 + -1 * d.weekday() + 2) % 7))
+                await ctx.send('Swim or Sink: Wednesday ' + '<t:' + 
+                    str(int(d.replace(day=sosday.day, hour=20, minute=0, second=0, microsecond=0).timestamp())) 
+                    + '>')
+            if "pg" in tourney.lower():
+                pgday = d + timedelta(days= ((7 + -1 * d.weekday() + 4) % 7))
+                await ctx.send('Proving Grounds: Friday ' + '<t:' + 
+                    str(int(d.replace(day=pgday.day, hour=20, minute=0, second=0, microsecond=0).timestamp())) 
+                    + '>')
+
+        #general case
         for i in range(7):
             #day of week
             day = d.strftime('%A')    
